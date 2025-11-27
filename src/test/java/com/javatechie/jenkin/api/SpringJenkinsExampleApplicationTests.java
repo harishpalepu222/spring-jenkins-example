759053c99776e0ff8e4f0c312de4b243bf2ce9c1
harishpalepu222 <harishpalepu222@gmail.com>
2025-11-27 18:37:43 +0530

    second commit

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsExampleApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsExampleApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsExampleApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing....");
+		logger.info("Test case executing second time....");
 		assertEquals(10, 5 + 5);
 	}
 }
